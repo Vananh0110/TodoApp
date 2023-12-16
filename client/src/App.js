eaa5dd985e121caf7db52862c3af7c5d198edc7d
Phạm Vân Anh <85979283+Vananh0110@users.noreply.github.com>
2023-12-16 07:56:42 +0700

    Complete App

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,17 @@
 import React, {Fragment} from 'react';
 import './App.css';
+import InputTodo from './components/InputTodo';
+import ListTodos from './components/ListTodos';
 
 function App() {
-  return <Fragment></Fragment>
+  return (
+    <Fragment>
+      <div className="container">
+        <InputTodo/>
+        <ListTodos/>
+      </div>
+    </Fragment>
+  )
 }
 
 export default App;
